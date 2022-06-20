07111b74a12f76ccec063e4cc7c5a3ce00a461c4
Raza <mraza6601@gmail.com>
2022-06-20 21:39:06 +0500

    shanu farmaish , without refresh new page opens

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 //import { Link } from "react-router-dom";<Link to='#shops' smooth>
 import { BrowserRouter, Link as Link1 } from "react-router-dom";
-import {HashLink as Link} from "react-router-hash-link";
+//import {HashLink as Link} from "react-router-hash-link";
 
 import logo from "/images/logoo.png";
 
 
 const NavBarItem = ({ title,link, classprops }) => (
-  <Link to={link} smooth > 
-  <li className={`hover:bg-[#2546bd] mx-12 rounded-full cursor-pointer ${classprops}`}>{title}</li></Link>
+  <Link1 to={link} > 
+  <li className={`hover:bg-[#2546bd] mx-12 rounded-full cursor-pointer ${classprops}`}>{title}</li></Link1>
 );
 
 
@@ -18,25 +18,17 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
 
   return (
-    <BrowserRouter>
+
     <nav className="w-full flex md:justify-center justify-between items-center px-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-      <Link to="/"><img src={logo}  alt="logo" className="w-20 cursor-pointer" /></Link>
+      <Link1 to="/"><img src={logo}  alt="logo" className="w-20 cursor-pointer" /></Link1>
         
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["About Us", "Services", "Salons", "Contact Us"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} link={`#${item}`} />
+        {["About Us", "Salons" , "login", "register" , "Contact Us"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} link={`/${item}`} />
         ))}
-        {/* <li className="bg-[#2952e3]  px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li> */}
 
-        <Link1 to="/login">
-        <li className="bg-[#2952e3]  px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
-        </Link1>
 
       </ul>
       <div className="flex relative">
@@ -59,7 +51,6 @@ const Navbar = () => {
         )}
       </div>
     </nav>
-    </BrowserRouter>
   );
 };
 
