bb6f773b882fe1b28ca47866ef8467c4757ea79e
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-07-12 10:18:05 +0500

    Special Button Updated

--- client/src/components/SpecialButton.jsx
+++ client/src/components/SpecialButton.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./SpecialButton.css"
 
 const SpecialButton = () => (
-    <div className="gradient-bg-welcome flex w-full justify-center items-center pb-10 ">
+    <div className="bg-white flex w-full justify-center items-center pb-10 mt-5">
         <Link to="/salons">
         <button
               type="button"
               onClick={()=>{}}
-              className="mt-20 w-[250px] flex flex-row justify-center items-center bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+              className="mt-20 w-[250px] flex flex-row justify-center items-center p-3 rounded-full cursor-pointer cta"
             >
 
-              <p className="text-white text-base font-semibold">
-                Show All the barbers shop
-              </p>
+              <span className="text-black text-base font-semibold">
+                Show All Barbers
+              </span>
+              <svg viewBox="0 0 13 10" height="10px" width="15px">
+              <path d="M1,5 L11,5"></path>
+              <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
             </button>
             </Link>
     </div>
