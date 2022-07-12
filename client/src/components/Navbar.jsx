4cc788070dcddc2957d854a9bd8c576b53ce70d3
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-07-12 12:15:39 +0500

    User Profile Updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -64,8 +64,8 @@ const Navbar = () => {
           alt="user-profile"
         />
         <p>
-          <span className="text-gray-400 text-14">Hi,</span>{" "}
-          <span className="text-gray-400 font-bold ml-1 text-14">Shayan</span>
+          <span className="text-gray-400 text-10 text-base">Hi,</span>{" "}
+          <span className="text-gray-400 font-bold ml-1 text-10 text-lg">Shayan</span>
         </p>
         <MdKeyboardArrowDown className="text-gray-400 text-14" />
       </div>
