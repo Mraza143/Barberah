bcae0ceec9c43937d830928078996400a069485d
Raza <mraza6601@gmail.com>
2022-07-13 22:40:41 +0500

    Appointment Error fixed

--- client/src/redux/actions/salonAction.js
+++ client/src/redux/actions/salonAction.js
@@ -5,13 +5,7 @@ import { ALL_SALON_REQUEST, ALL_SALON_SUCCESS, ALL_SALON_FAIL } from "../constan
 export const getAllSalons = () => async(dispatch) => {
     try {
         dispatch({ type: ALL_SALON_REQUEST })
-            //const config = { headers: { 'Content-Type': 'application/json' } }
-            //const  resp  = await axios.get(`http://localhost:5000/api/salons`)
-            //const resp = await fetch(`http://localhost:5000/api/SALONs/${name}`);
 
-        //
-        //const salons = await resp.json();
-        // const  salons  = await axios.get(`http://localhost:5000/api/salons`)
 
         const { data } = await axios.get(`http://localhost:5000/api/salons`);
         //   const salons = await data.json();
--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -19,6 +19,7 @@ const reviewSchema = mongoose.Schema(
       average:{
         type:Number,
         
+        
       }
   },
   {
