5c3071f4bf6e72f03cb20d385cdb8042a1a9d4fc
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-07-14 17:50:47 +0500

    Responsiveness Added to About Us Page

--- client/src/components/aboutUs.jsx
+++ client/src/components/aboutUs.jsx
@@ -9,15 +9,13 @@ const AboutUs = () => {
                 <img src="./images/about-us.jpg" className="w-screen"></img>
                 <div className="centered text-white text-5xl text-center">EMPOWERING BARBERS SINCE 2021</div>
             </div>
-            <div>
-            </div>
             <div className="flex flex-row flex-wrap">
-                <div className="w-1/2 h-fit mt-16 mx-20 mb-0">
-                    <div className="px-8 flex justify-center flex-col text-left mx-16 rounded-lg">
-                        <h1 className="py-6 text-3xl">FOR BARBERS</h1>
+                <div className="lg:w-1/2 h-fit lg:mt-16 lg:mx-20 lg:mb-0">
+                    <div className="px-4 lg:px-8 flex justify-center flex-col text-left mx-8 my-8 rounded-lg">
+                        <h1 className="py-4 text-3xl">FOR BARBERS</h1>
                         <p className="pb-4">More Than Just An Appointment Booking App, It’s Life Made Easy!</p>
                         <p className="pb-2 text-lg font-semibold">Features</p>
-                        <ol class="list-disc px-6">
+                        <ol class="list-disc lg:px-6">
                             <li>Mobile and Web version</li>
                             <li>Reduce No Shows with the reminders feature</li>
                             <li>Get paid for deposits and services using</li>
@@ -34,17 +32,17 @@ const AboutUs = () => {
                         </ol>
                     </div>
                 </div>
-                <div className="mt-16">
-                    <img src="/images/mobile.png" className="h-5/6" />
+                <div className="hidden lg:flex lg:mt-16">
+                    <img src="/images/mobile.png" className="lg:h-5/6" />
                 </div>
             </div>
             <div className="flex flex-row flex-wrap w-full bg-[#37c7da] text-white">
-                <div className="mt-16 ml-36 mb-0 pb-0">
-                    <img src="/images/mobile.png" className="h-4/6" />
+                <div className="hidden lg:flex lg:mt-16 lg:pl-36 lg:mb-0 lg:pb-0">
+                    <img src="/images/mobile.png" className="lg:h-4/6" />
                 </div>
-                <div className="w-1/2 h-fit mt-16 mx-20 mb-0">
-                    <div className="px-8 flex justify-center flex-col text-left mx-16 rounded-lg ml-20">
-                        <h1 className="py-6 text-3xl">FOR CLIENTS</h1>
+                <div className="lg:w-1/2 h-fit lg:mt-16 lg:mx-20 lg:mb-0">
+                    <div className="px-4 lg:px-8 flex justify-center flex-col text-left mx-4 my-4 lg:mx-16 rounded-lg lg:pl-20">
+                        <h1 className="py-4 text-3xl">FOR CLIENTS</h1>
                         <p className="pb-4">More Than Just An Appointment Booking App, It’s Life Made Easy!</p>
                         <p className="pb-2 text-lg font-semibold">Features</p>
                         <ol class="list-disc px-6">
